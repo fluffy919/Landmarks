d8a6610a6ab2844808f6b41ad8ce0c129797e99f
mobileANDweb <mobiledeveloper112075@gmail.com>
2021-05-03 11:46:48 -0400

    SwiftUI Tutorial

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by B on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
